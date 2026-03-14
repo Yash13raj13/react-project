9692a591783b60a828d942f6a2ed79a3db5c8ab3
yash13raj13 <yash.04514815623@cseaiml.mait.ac.in>
2026-03-14 23:07:32 +0530

    typescript

---
+++ src/components/Navbar.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar: React.FC = () => {
+  return (
+    <nav style={{ padding: "10px", background: "#111", color: "white" }}>
+      <h2>Music Theory App</h2>
+    </nav>
+  );
+};
+
+export default Navbar;
---
+++ src/pages/Home.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Home: React.FC = () => {
+  return (
+    <div>
+      <h1>Welcome to Music Theory Learning</h1>
+      <p>Learn instruments, theory and practice daily.</p>
+    </div>
+  );
+};
+
+export default Home;
